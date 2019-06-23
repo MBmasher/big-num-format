f9a50db10fcb46a11043a5002caa2e81a2b43fc4
MBmasher <mbmasher@gmail.com>
2019-06-23 12:22:49 +1000

    change version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="big-num-format",
-    version="0.1.2",
+    version="0.1.3",
     scripts=[],
     author="MBmasher",
     author_email="mbmasher@gmail.com",
